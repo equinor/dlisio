e2b75d49951da4a42cf8cd78e0eaaf8e88e4b41a
Jørgen Kvalsvik <jokva@statoil.com>
2018-08-07 16:27:42 +0200

    Add representation-code table in types.h

--- lib/include/dlisio/types.h
+++ lib/include/dlisio/types.h
@@ -81,6 +81,34 @@ const char* dlis_attref( const char*, int32_t* ident1_len,
 const char* dlis_status( const char*, uint8_t* );
 const char* dlis_units( const char*, uint8_t*, char* );
 
+#define DLIS_FSHORT 1  // Low precision floating point
+#define DLIS_FSINGL 2  // IEEE single precision floating point
+#define DLIS_FSING1 3  // Validated single precision floating point
+#define DLIS_FSING2 4  // Two-way validated single precision floating point
+#define DLIS_ISINGL 5  // IBM single precision floating point
+#define DLIS_VSINGL 6  // VAX single precision floating point
+#define DLIS_FDOUBL 7  // IEEE double precision floating point
+#define DLIS_FDOUB1 8  // Validated double precision floating point
+#define DLIS_FDOUB2 9  // Two-way validated double precision floating point
+#define DLIS_CSINGL 10 // Single precision complex
+#define DLIS_CDOUBL 11 // Double precision complex
+#define DLIS_SSHORT 12 // Short signed integer
+#define DLIS_SNORM  13 // Normal signed integer
+#define DLIS_SLONG  14 // Long signed integer
+#define DLIS_USHORT 15 // Short unsigned integer
+#define DLIS_UNORM  16 // Normal unsigned integer
+#define DLIS_ULONG  17 // Long unsigned integer
+#define DLIS_UVARI  18 // Variable-length unsigned integer
+#define DLIS_IDENT  19 // Variable-length identifier
+#define DLIS_ASCII  20 // Variable-length ASCII character string
+#define DLIS_DTIME  21 // Date and time
+#define DLIS_ORIGIN 22 // Origin reference
+#define DLIS_OBNAME 23 // Object name
+#define DLIS_OBJREF 24 // Object reference
+#define DLIS_ATTREF 25 // Attribute reference
+#define DLIS_STATUS 26 // Boolean status
+#define DLIS_UNITS  27 // Units expression
+
 #ifdef __cplusplus
 }
 #endif
