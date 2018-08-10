c87eb7334c2bffaf0042f54e51d2a3d5a9a85b3a
Jørgen Kvalsvik <jokva@statoil.com>
2018-08-10 12:30:27 +0200

    Expose array-subscript file handle
    This functions, with the sul and formatting members, exposes transparent
    reading of full logical records, without segments, sufficient for
    writing arbitrary parsing routines on top of the C++ base.

--- python/dlisio/__init__.py
+++ python/dlisio/__init__.py
@@ -1,1 +1,23 @@
 __version__ = '0.0.0'
+
+from . import core
+
+def load(path):
+    return dlis(path)
+
+class dlis(object):
+    def __init__(self, path):
+        self.fp = core.file(path)
+        self.sul = self.fp.sul()
+
+        self.bookmarks = []
+        self.formatting = []
+        off = 0
+        while not self.fp.eof():
+            pos, off, explicit = self.fp.mark(off)
+
+            self.formatting.append(explicit)
+            self.bookmarks.append(pos)
+
+    def __getitem__(self, i):
+        return self.fp.getrecord(self.bookmarks[i])
