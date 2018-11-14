c544693d0812fbc74fb3efcec51957c10be6ccf4
Jørgen Kvalsvik <jokva@equinor.com>
2018-11-14 21:30:13 +0100

    Use custom type and stringmaker for bytes in test
    Add a custom, test-specific byte-array type to more ergonomically build
    tests that involve writing and comparing byte arrays.  Catch otherwise
    would interprets byte arrays as C-strings. Do the sensible thing and
    print as hex instead.

--- lib/test/types.cpp
+++ lib/test/types.cpp
@@ -1,14 +1,115 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
+#include <cstring>
+#include <initializer_list>
+#include <iomanip>
 #include <limits>
+#include <sstream>
 #include <string>
 
 #include <catch2/catch.hpp>
 
 #include <dlisio/types.h>
 
+/*
+ * Custom type for byte arrays, for nicely printing mismatch between expected
+ * and actual parameters to comparison of byte arrays.
+ *
+ * This type exists mostly for Catch's stringmaker to have something to hook
+ * into, as it relies on the type system to pick the correct specialisation.
+ * Using std::array is pretty clumsy, and (unsigned) char* already means
+ * "print-as-string".
+ */
+template< std::size_t N, typename T = char >
+struct bytes {
+    using value_type = T[N];
+
+    bytes() = default;
+    bytes( const value_type& xs ) {
+        using std::begin;
+        using std::end;
+        std::copy( begin( xs ), end( xs ), begin( this->data ) );
+    }
+
+    bytes( std::initializer_list< std::size_t > xs ) {
+        using std::begin;
+        using std::end;
+        std::copy( begin( xs ), end( xs ), begin( this->data ) );
+    }
+
+    operator value_type& ()             { return this->data; }
+    operator const value_type& () const { return this->data; }
+
+    value_type data;
+};
+
+namespace {
+
+template< typename T, std::size_t N >
+class BytesEqualsMatcher : public Catch::MatcherBase< bytes< N > > {
+public:
+    explicit BytesEqualsMatcher( T x ) : lhs( x ) {}
+
+    virtual bool match( const bytes< N >& rhs ) const override {
+        static_assert( sizeof( T ) == N, "sizeof(T) must be equal to N" );
+        return std::memcmp( &this->lhs, rhs, sizeof( T ) ) == 0;
+    }
+
+    virtual std::string describe() const override {
+        bytes< N > lhsb;
+        std::memcpy( lhsb, &this->lhs, N );
+
+        using str = Catch::StringMaker< bytes< N > >;
+        return "== " + str::convert( lhsb );
+    }
+
+private:
+    T lhs;
+};
+
+template< typename T >
+BytesEqualsMatcher< T, sizeof( T ) > BytesEquals( T lhs ) {
+    return BytesEqualsMatcher< T, sizeof( T ) >{ lhs };
+}
+
+}
+
+namespace Catch {
+
+/*
+ * print bytes as a hex sequence
+ */
+template< std::size_t N >
+struct StringMaker< bytes< N > > {
+    static std::string convert( const bytes< N >& xs ) {
+        std::stringstream ss;
+        ss << std::hex;
+
+        /*
+         * Convert every byte to unsigned char (even though the byte array
+         * stores as unspecified signed) to preserve representation as a byte,
+         * so it doesn't change once widened to an int. Once an int, the
+         * std::hex manipulator picks it up and writes it as a hex value
+         */
+        for( unsigned char x : xs.data ) {
+            ss << std::setfill( '0' )
+               << std::setw( 2 )
+               << int( x ) << ' '
+               ;
+        }
+
+        auto s = ss.str();
+        /* remove trailing space */
+        s.pop_back();
+        return s;
+    }
+};
+
+}
+
 TEST_CASE("signed short (8-bit)", "[type]") {
-    const std::array< unsigned char[1], 7 > inputs = {{
+    const std::array< bytes< 1 >, 7 > inputs = {{
         { 0x00 }, // 0
         { 0x01 }, // 1
         { 0x59 }, // 89
@@ -36,7 +137,7 @@ TEST_CASE("signed short (8-bit)", "[type]") {
 }
 
 TEST_CASE("signed normal (16-bit)", "[type]") {
-    const std::array< unsigned char[2], 8 > inputs = {{
+    const std::array< bytes< 2 >, 8 > inputs = {{
         { 0x00, 0x00 }, // 0
         { 0x00, 0x01 }, // 1
         { 0x00, 0x59 }, // 89
@@ -66,7 +167,7 @@ TEST_CASE("signed normal (16-bit)", "[type]") {
 }
 
 TEST_CASE("signed long (32-bit)", "[type]") {
-    const std::array< unsigned char[4], 8 > inputs = {{
+    const std::array< bytes< 4 >, 8 > inputs = {{
         { 0x00, 0x00, 0x00, 0x00, }, // 0
         { 0x00, 0x00, 0x00, 0x01, }, // 1
         { 0x00, 0x00, 0x00, 0x59, }, // 89
@@ -96,7 +197,7 @@ TEST_CASE("signed long (32-bit)", "[type]") {
 }
 
 TEST_CASE("unsigned short (8-bit)", "[type]") {
-    const std::array< unsigned char[1], 7 > inputs = {{
+    const std::array< bytes< 1 >, 7 > inputs = {{
         { 0x00 }, // 0
         { 0x01 }, // 1
         { 0x59 }, // 89
@@ -124,7 +225,7 @@ TEST_CASE("unsigned short (8-bit)", "[type]") {
 }
 
 TEST_CASE("unsigned normal (16-bit)", "[type]") {
-    const std::array< unsigned char[2], 8 > inputs = {{
+    const std::array< bytes< 2 >, 8 > inputs = {{
         { 0x00, 0x00 }, // 0
         { 0x00, 0x01 }, // 1
         { 0x00, 0x59 }, // 89
@@ -154,7 +255,7 @@ TEST_CASE("unsigned normal (16-bit)", "[type]") {
 }
 
 TEST_CASE("unsigned long (32-bit)", "[type]") {
-    const std::array< unsigned char[4], 8 > inputs = {{
+    const std::array< bytes< 4 >, 8 > inputs = {{
         { 0x00, 0x00, 0x00, 0x00, }, // 0
         { 0x00, 0x00, 0x00, 0x01, }, // 1
         { 0x00, 0x00, 0x00, 0x59, }, // 89
