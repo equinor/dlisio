0cad6fd1c83dcc2d6898107aeadb7e3c6f7238cc
Jørgen Kvalsvik <jokva@equinor.com>
2018-12-11 13:27:22 +0100

    Add enum-class for representation codes

--- lib/extension/dlisio/ext/types.hpp
+++ lib/extension/dlisio/ext/types.hpp
@@ -9,6 +9,36 @@
 
 namespace dl {
 
+enum class representation_code : std::uint8_t {
+    fshort = DLIS_FSHORT,
+    fsingl = DLIS_FSINGL,
+    fsing1 = DLIS_FSING1,
+    fsing2 = DLIS_FSING2,
+    isingl = DLIS_ISINGL,
+    vsingl = DLIS_VSINGL,
+    fdoubl = DLIS_FDOUBL,
+    fdoub1 = DLIS_FDOUB1,
+    fdoub2 = DLIS_FDOUB2,
+    csingl = DLIS_CSINGL,
+    cdoubl = DLIS_CDOUBL,
+    sshort = DLIS_SSHORT,
+    snorm  = DLIS_SNORM,
+    slong  = DLIS_SLONG,
+    ushort = DLIS_USHORT,
+    unorm  = DLIS_UNORM,
+    ulong  = DLIS_ULONG,
+    uvari  = DLIS_UVARI,
+    ident  = DLIS_IDENT,
+    ascii  = DLIS_ASCII,
+    dtime  = DLIS_DTIME,
+    origin = DLIS_ORIGIN,
+    obname = DLIS_OBNAME,
+    objref = DLIS_OBJREF,
+    attref = DLIS_ATTREF,
+    status = DLIS_STATUS,
+    units  = DLIS_UNITS,
+};
+
 struct datetime {
     int Y, TZ, M, D, H, MN, S, MS;
 };
