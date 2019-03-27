6025a02ae7f878e1249e70e8c5b16e7fccaf1b53
Jørgen Kvalsvik <jokva@equinor.com>
2019-03-27 09:00:09 +0100

    Rename test-fixtures to match file names
    Rename from f and channel_f to DWL206 and only_channels respectively.

--- python/tests/__init__.py
+++ python/tests/__init__.py
@@ -2,12 +2,12 @@ import pytest
 
 import dlisio
 
-@pytest.fixture(scope="module", name="f")
-def load_206_DWL():
+@pytest.fixture(scope="module", name="DWL206")
+def DWL206():
     with dlisio.load('data/206_05a-_3_DWL_DWL_WIRE_258276498.DLIS') as f:
         yield f
 
-@pytest.fixture(scope="module", name="channels_f")
-def load_only_channels():
+@pytest.fixture(scope="module", name="only_channels")
+def only_channels():
     with dlisio.load('data/only-channels.dlis') as f:
         yield f
