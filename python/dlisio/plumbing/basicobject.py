56e53a467b2ecfefa6919c5689828ce04153a9ff
Erlend Hårstad <erha@equinor.com>
2019-06-12 12:04:17 +0200

    Make type and attic properties -> attriutes
    Expose the underlying attribute directly rather than through a property
    that only forwards the underlying attribute anyways.

--- python/dlisio/plumbing/basicobject.py
+++ python/dlisio/plumbing/basicobject.py
@@ -51,11 +51,11 @@ class BasicObject():
     """
 
     def __init__(self, obj, name = None, type = None):
-        self._type      = type
+        self.type       = type
         self.name       = None
         self.origin     = None
         self.copynumber = None
-        self._attic     = obj
+        self.attic      = obj
 
         #: Dictionary with all unexpected attributes.
         #: While all expected attributes are presented as members of object
@@ -104,40 +104,6 @@ class BasicObject():
                                 self.name,
                                 self.origin,
                                 self.copynumber)
-
-    @property
-    def type(self):
-        """ Type
-
-        The type of the object. Together, the type- and name-attribute form a
-        unique object-identifier across all objects in the Logical File
-
-        Returns
-        -------
-
-        type : str
-        """
-        return self._type
-
-    @property
-    def attic(self):
-        """ The original object as represented on disk
-
-        Attic refers the underlying basic_object, which is a
-        dict-representation of the data on disk. The attic can be None if this
-        particular instance was not loaded from disk.
-
-        Notes
-        -----
-
-        This attribute is mainly intended for debugging purpuses
-
-        Returns
-        -------
-
-        attic : dl:basic_object
-        """
-        return self._attic
 
     def update_stash(self, label, value):
         """ Stashes attributes
