dcac337001e0590caa751b2bd6d31c1c82c0044a
Erlend Hårstad <erha@equinor.com>
2019-10-08 12:55:39 +0200

    Rename sub-directories in python/data/
    The test-file directories always reflected which chapter the files aimed
    to test, but that was not so obvious from the naming.

--- python/tests/test_layout.py
+++ python/tests/test_layout.py
@@ -58,59 +58,59 @@ def test_sul_error_values():
     assert sul['layout'] == 'unknown'
 
 def test_load_pre_sul_garbage():
-    with dlisio.load('data/layout/pre-sul-garbage.dlis') as (f,):
+    with dlisio.load('data/chap2/pre-sul-garbage.dlis') as (f,):
         assert f.storage_label() == f.storage_label()
         assert f.sul_offset == 12
 
 def test_notdlis():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/nondlis.txt')
+        _ = dlisio.load('data/chap2/nondlis.txt')
     assert "could not find storage label" in str(excinfo.value)
 
 def test_broken_sul():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/incomplete-sul.dlis')
+        _ = dlisio.load('data/chap2/incomplete-sul.dlis')
     assert "file may be corrupted" in str(excinfo.value)
 
 def test_load_pre_vrl_garbage():
-    with dlisio.load('data/layout/pre-sul-pre-vrl-garbage.dlis') as (f,):
+    with dlisio.load('data/chap2/pre-sul-pre-vrl-garbage.dlis') as (f,):
         assert f.storage_label() == f.storage_label()
         assert f.sul_offset == 12
 
 def test_old_vrs():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/old-vr.dlis')
+        _ = dlisio.load('data/chap2/old-vr.dlis')
     assert "could not find visible record" in str(excinfo.value)
 
 def test_broken_vr():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/incomplete-vr.dlis')
+        _ = dlisio.load('data/chap2/incomplete-vr.dlis')
     assert "file may be corrupted" in str(excinfo.value)
 
 def test_load_small_file():
     # <4K files infinite loop bug check
-    with dlisio.load('data/layout/small.dlis'):
+    with dlisio.load('data/chap2/small.dlis'):
         pass
 
 def test_load_7K_file_with_several_LR():
     # 4K-8K files infinite loop bug check
-    with dlisio.load('data/layout/7K-file.dlis'):
+    with dlisio.load('data/chap2/7K-file.dlis'):
         pass
 
 def test_truncated():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/truncated.dlis')
+        _ = dlisio.load('data/chap2/truncated.dlis')
     assert "file truncated" in str(excinfo.value)
 
 def test_too_small_record():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/too-small-record.dlis')
+        _ = dlisio.load('data/chap2/too-small-record.dlis')
     assert "in record 0 corrupted" in str(excinfo.value)
 
 def test_padbytes_as_large_as_record():
     # 180-byte long explicit record with padding, and padbytes are set to 180
     # (leaving the resulting len(data) == 0)
-    f = dlisio.open('data/layout/padbytes-large-as-record.dlis')
+    f = dlisio.open('data/chap2/padbytes-large-as-record.dlis')
     try:
         f.reindex([0], [180])
 
@@ -123,7 +123,7 @@ def test_padbytes_as_large_as_record():
 def test_padbytes_as_large_as_segment():
     # 180-byte long explicit record with padding, and padbytes are set to 176
     # record is expected to not be present
-    f = dlisio.open('data/layout/padbytes-large-as-segment-body.dlis')
+    f = dlisio.open('data/chap2/padbytes-large-as-segment-body.dlis')
     try:
         f.reindex([0], [180])
 
@@ -134,20 +134,20 @@ def test_padbytes_as_large_as_segment():
 
 def test_bad_padbytes():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/padbytes-bad.dlis')
+        _ = dlisio.load('data/chap2/padbytes-bad.dlis')
     assert "bad segment trim" in str(excinfo.value)
 
 def test_padbytes_encrypted():
-    with dlisio.load('data/layout/padbytes-encrypted.dlis'):
+    with dlisio.load('data/chap2/padbytes-encrypted.dlis'):
         pass
 
 def test_load_fdata_VR_aligned():
-    with dlisio.load('data/layout/fdata-vr-aligned.dlis') as (f, *_):
+    with dlisio.load('data/chap2/fdata-vr-aligned.dlis') as (f, *_):
         assert len(f.fdata_index) == 1
         assert f.fdata_index['T.FRAME-I.DLIS-FRAME-O.3-C.1'] == [0]
 
 def test_load_fdata_many_in_same_VR():
-    with dlisio.load('data/layout/fdata-many-in-same-vr.dlis') as (f, *_):
+    with dlisio.load('data/chap2/fdata-many-in-same-vr.dlis') as (f, *_):
         assert len(f.fdata_index) == 2
         assert f.fdata_index['T.FRAME-I.DLIS-FRAME-O.3-C.1'] == [0, 1]
         ident = '3'*255
@@ -155,18 +155,18 @@ def test_load_fdata_many_in_same_VR():
         assert f.fdata_index[fingerprint] == [3]
 
 def test_3lrs_in_lr_in_vr():
-    with dlisio.load('data/layout/example-record.dlis'):
+    with dlisio.load('data/chap2/example-record.dlis'):
         pass
 
 def test_2_lr_in_vr():
-    with dlisio.load('data/layout/2lr-in-vr.dlis'):
+    with dlisio.load('data/chap2/2lr-in-vr.dlis'):
         pass
 
 def test_lr_in_2vrs():
-    with dlisio.load('data/layout/lr-in-2vrs.dlis'):
+    with dlisio.load('data/chap2/lr-in-2vrs.dlis'):
         pass
 
 def test_vrl_and_lrsh_mismatch():
     with pytest.raises(RuntimeError) as excinfo:
-        _ = dlisio.load('data/layout/wrong-lrhs.dlis')
+        _ = dlisio.load('data/chap2/wrong-lrhs.dlis')
     assert "visible record/segment inconsistency" in str(excinfo.value)
