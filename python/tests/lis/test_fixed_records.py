61d329681d73d63279db2f160bf8b3794b19c293
Erlend Hårstad <erha@equinor.com>
2021-02-18 13:51:08 +0100

    Add python tests for LIS RHLR/RTLR/THLR/TTLR

--- python/tests/lis/test_fixed_records.py
+++ python/tests/lis/test_fixed_records.py
@@ -25,3 +25,51 @@ def test_file_trailer():
         assert trailer.max_pr_length       == ' 1024'
         assert trailer.file_type           == ' '*2
         assert trailer.next_file_name      == ' '*10
+
+def test_reel_header():
+    with lis.load('data/lis/MUD_LOG_1.LIS') as (f, *tail):
+        reel = f.reel.header()
+
+        assert reel.service_name        == ' '*6
+        assert reel.date                == '09/11/17'
+        assert reel.origin_of_data      == ' '*4
+        assert reel.name                == 'Georeel '
+        assert reel.continuation_number == '01'
+        assert reel.prev_reel_name      == ' '*8
+        assert reel.comment             == ' '*74
+
+def test_reel_trailer():
+    with lis.load('data/lis/MUD_LOG_1.LIS') as (f, *tail):
+        reel = f.reel.trailer()
+
+        assert reel.service_name        == ' '*6
+        assert reel.date                == '09/11/17'
+        assert reel.origin_of_data      == ' '*4
+        assert reel.name                == 'Georeel '
+        assert reel.continuation_number == '01'
+        assert reel.next_reel_name      == ' '*8
+        assert reel.comment             == ' '*74
+
+def test_tape_header():
+    with lis.load('data/lis/MUD_LOG_1.LIS') as (f, *tail):
+        tape = f.tape.header()
+
+        assert tape.service_name        == ' '*6
+        assert tape.date                == ' '*8
+        assert tape.origin_of_data      == ' '*4
+        assert tape.name                == 'Geotape '
+        assert tape.continuation_number == '01'
+        assert tape.prev_tape_name      == ' '*8
+        assert tape.comment             == ' '*74
+
+def test_tape_trailer():
+    with lis.load('data/lis/MUD_LOG_1.LIS') as (f, *tail):
+        tape = f.tape.trailer()
+
+        assert tape.service_name        == ' '*6
+        assert tape.date                == ' '*8
+        assert tape.origin_of_data      == ' '*4
+        assert tape.name                == 'Geotape '
+        assert tape.continuation_number == '01'
+        assert tape.next_tape_name      == ' '*8
+        assert tape.comment             == ' '*74
