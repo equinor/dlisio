c2287807a56a7952201a54402f80a0e64c3c4938
Erlend Hårstad <erha@equinor.com>
2021-02-25 08:52:29 +0100

    Use 'furo' as sphinx theme
    This commit changes the theme on readthedocs to 'furo'. furo is a more
    minimalistic and modern theme than the default readthedocs theme. It's
    used by both pip and urllib3, among others.
    
    [1] https://github.com/pradyunsg/furo

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -138,7 +138,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+try:
+    __import__('furo')
+    html_theme = 'furo'
+except ImportError:
+    pass
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
